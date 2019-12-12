2b6a897365dfed4362eebfbf013917e40b5aa295
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-12-12 20:52:28 +0900

    コントローラーに記述

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index()
+    {
+      $data = [
+        'msg' => 'this is sample message',
+      ];
+      return view('hello.index', $data);
+    }
 }
